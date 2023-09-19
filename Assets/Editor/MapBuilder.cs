7bc8a03c788ba7179eb84c6c403969282bca4072
EvgenieMyasnikov <e.myasnikov@carx-tech.com>
2023-09-19 11:05:12 +0300

    Правки, очистка кэша карт после создания новой

--- Assets/Editor/MapBuilder.cs
+++ Assets/Editor/MapBuilder.cs
@@ -47,20 +47,12 @@ namespace Editor
             var titleIconPath = Application.dataPath.Substring(0, Application.dataPath.Length - 6)
                                 + AssetDatabase.GetAssetPath(MapMetaConfig.Value.largeIcon);
             
-            SteamClient.Shutdown();
-            SteamClient.Init(SteamUGCManager.APP_ID, false);
-            var steamUgc = new SteamUGCManager();
-            EditorApplication.update += steamUgc.Update;
-            
-            EditorCoroutineUtility.StartCoroutine(steamUgc.CreatePublisherItem(MapMetaConfig.Value.mapName, titleIconPath, 
-                item =>
-            {
-                var mapScene = EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Additive);
+            var mapScene = EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Additive);
 
-                m_cacheDataList.Clear();
-                m_cacheData = new GameObject("CacheData", typeof(CacheData)).GetComponent<CacheData>();
+            m_cacheDataList.Clear();
+            m_cacheData = new GameObject("CacheData", typeof(CacheData)).GetComponent<CacheData>();
 
-                ValidComponents(root.transform, null, "Garbage",
+            ValidComponents(root.transform, null, "Garbage",
                 typeof(Transform), 
                 typeof(MeshCollider), 
                 typeof(BoxCollider),
@@ -75,27 +67,43 @@ namespace Editor
                 typeof(MapConfig),
                 typeof(CacheData));
             
-                for (int i = 0; i < sceneObjects.Length; i++)
-                {
-                    sceneObjects[i].transform.SetParent(null);
-                }
+            for (int i = 0; i < sceneObjects.Length; i++)
+            {
+                sceneObjects[i].transform.SetParent(null);
+            }
             
-                m_cacheData.gameMarkers = new List<GameMarkerData>(m_cacheDataList.ToArray());
+            m_cacheData.gameMarkers = new List<GameMarkerData>(m_cacheDataList.ToArray());
             
-                DestroyImmediate(root);
+            DestroyImmediate(root);
             
-                var scenePath = path + "/" + MapMetaConfig.Value.mapName + item.FileId.Value.ToString() + ".unity";
-            
-                EditorSceneManager.SaveScene(mapScene, scenePath);
-                SceneManager.UnloadScene(mapScene);
+            var scenePath = path + "/" + MapMetaConfig.Value.mapName + ".unity";
+                
+            foreach (var file in Directory.GetFiles(path))
+            {
+                File.Delete(file);
+            }
+                
+            EditorSceneManager.SaveScene(mapScene, scenePath);
+            SceneManager.UnloadScene(mapScene);
 
-                if (Directory.Exists(assetManifestPath))
-                {
-                    Directory.Delete(assetManifestPath, true);
-                }
+            if (Directory.Exists(assetManifestPath))
+            {
+                Directory.Delete(assetManifestPath, true);
+            }
 
-                Directory.CreateDirectory(assetManifestPath);
+            Directory.CreateDirectory(assetManifestPath);
             
+            SteamClient.Shutdown();
+            SteamClient.Init(SteamUGCManager.APP_ID, false);
+            var steamUgc = new SteamUGCManager();
+            EditorApplication.update += steamUgc.Update;
+            
+            EditorCoroutineUtility.StartCoroutine(steamUgc.CreatePublisherItem(MapMetaConfig.Value.mapName, titleIconPath, item =>
+            {
+                var scenePathNew = path + "/" + MapMetaConfig.Value.mapName + item.FileId.Value + ".unity";
+                AssetDatabase.RenameAsset(scenePath, MapMetaConfig.Value.mapName + item.FileId.Value);
+                scenePath = scenePathNew;
+                
                 var bundleBuilds = CreateBundleArrayDataForOneElement(MapMetaConfig.Value.mapName, scenePath);
                 BuildPipeline.BuildAssetBundles(assetManifestPath,
                     bundleBuilds, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
@@ -107,7 +115,7 @@ namespace Editor
                 void Callback(ulong id)
                 {
                     Directory.Delete(assetDir + "Standalone", true);
-                
+
                     if (id == SteamUGCManager.PUBLISH_ITEM_FAILED_CODE)
                     {
                         Debug.LogError("Publish failed");
