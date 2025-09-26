afeb4b3c13333a4eb7fd516226c221dd41fddf09
Jeefrect <121081064+Jeefrect@users.noreply.github.com>
2025-09-26 05:08:11 +0200

    Fix ComponentUtility.cs
    Пофиксил FindAllComponent, выкидывал стрёмные NRE

--- Assets/Editor/ComponentUtility.cs
+++ Assets/Editor/ComponentUtility.cs
@@ -3,41 +3,60 @@ using UnityEngine;
 
 namespace Editor
 {
-    public static class ComponentUtility
-    {
-        public static T[] FindAllComponent<T>(this Transform parent, params Component[] validNames) where T : Component
-        {
-            var components = new List<T>(parent.childCount);
-            for (int i = 0; i < parent.childCount; i++)
-            {
-                var child = parent.GetChild(i);
-                var childChild = child.FindAllComponent<T>();
-                if (childChild != null && childChild.Length > 0)
-                {
-                    components.AddRange(childChild);
-                }
+	public static class ComponentUtility
+	{ 
+		public static T[] FindAllComponent<T>(this Transform parent, params Component[] validNames) where T : Component
+		{
+			if (parent == null)
+				return System.Array.Empty<T>();
 
-                var component = child.GetComponent<T>();
-                if (component != null)
-                {
-                    bool validName = false;
-                    foreach (var name in validNames)
-                    {
-                        if (component.transform.name == name.transform.name)
-                        {
-                            validName = true;
-                            break;
-                        }
-                    }
+			var components = new List<T>(parent.childCount);
+			
+			bool acceptAll = validNames == null || validNames.Length == 0;
 
-                    if (validName)
-                    {
-                        components.Add(component);
-                    }
-                }
-            }
+			for (int i = 0; i < parent.childCount; i++)
+			{
+				var child = parent.GetChild(i);
+				var childChild = child.FindAllComponent<T>(validNames);
 
-            return components.ToArray();
-        }
-    }
+				if (childChild != null && childChild.Length > 0)
+				{
+					components.AddRange(childChild);
+				}
+				
+				var component = child.GetComponent<T>();
+				if (component == null)
+				{
+					continue;
+				}
+
+				bool validName = false;
+				if (acceptAll)
+				{
+					validName = true;
+				}
+				else
+				{
+					foreach (var name in validNames)
+					{
+						if (name == null || name.transform == null || component.transform == null)
+							continue;
+						
+						if (component.transform.name == name.transform.name)
+						{
+							validName = true;
+							break;
+						}
+					}
+				}
+
+				if (validName)
+				{
+					components.Add(component);
+				}
+			}
+
+			return components.ToArray();
+		}
+	}
 }
